9b49a0d00e266a4d79dc9e019d83767b57e61f8a
Julian <julian.kucia@gmail.com>
2026-04-04 19:16:54 +0200

    column dimensions

--- generator_harmonogramu.py
+++ generator_harmonogramu.py
@@ -1,5 +1,5 @@
 import openpyxl as op
-
+from openpyxl.styles import Alignment
 
 wb = op.Workbook()
 ws = wb.active
@@ -8,6 +8,8 @@ ws['A1'] = 42
 
 for i in range(101):
     column = i + 2
-    ws.cell(row = 10, column = column, value = i)
+    cell = ws.cell(row = 10, column = column, value = i)
+    cell.alignment = Alignment(horizontal = 'center')
+    ws.column_dimensions[op.utils.cell.get_column_letter(column)].width = 3
 
 wb.save("harmonogram.xlsx")
